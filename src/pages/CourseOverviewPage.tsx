469259df0c373f05e0a5387ebba1fbdb79879a83
Daria Cheatham <daria.cheatham@gmail.com>
2023-11-07 19:39:35 -0800

    Wrong tool for the job.

--- src/pages/CourseOverviewPage.tsx
+++ src/pages/CourseOverviewPage.tsx
@@ -40,105 +40,11 @@ export default function CourseOverviewPage()
 
 export function DropRateBump( props: { data: Courses | null })
 {
-   const data = computeDropRateBumpData(props.data);
-   return <div style={{width: "100%", height: 200 + 50 * size(data)}}>
-    <ResponsiveBump
-        theme={theme}
-        data={data}
-        colors={{ scheme: 'spectral' }}
-        lineWidth={3}
-        activeLineWidth={6}
-      axisBottom={{
-          tickSize: 5,
-          tickPadding: 5,
-          tickRotation: 0,
-          legend: '',
-          legendPosition: 'middle',
-          legendOffset: 32
-      }}
-      axisLeft={{
-          tickSize: 5,
-          tickPadding: 5,
-          tickRotation: 0,
-          legend: 'Drop Rate',
-          legendPosition: 'middle',
-          legendOffset: -40,
-          format: '.00%',
-      }}
-      margin={{ top: 40, right: 100, bottom: 40, left: 60 }}
-    />
+   return <div style={{width: "100%", height: 600}}>
     </div>
 }
 
 
-interface EnrollmentBumpData {
-  total_headcount: number;
-  total_dropped: number;
-  course_ids: number[];
-}
-
-function computeDropRateBumpData(coursedata: Courses | null) {
-  const groupedByInstructor = groupEnrollmentByInstructor(coursedata);
-
-  const offeredTimes = uniq(map(coursedata?.courses, (course) => `${course.year}-${course.semester}`));
-
-  const data = map(groupedByInstructor, (instructorData, instructor) => {
-    return {
-      id: instructor,
-      data: computeDropRateBumpDataForInstructor(offeredTimes, instructorData)
-    }
-  });
-
-  console.log(data)
-  return data;
-}
-
-
-function computeDropRateBumpDataForInstructor(offeredTimes: string[], instructorData: Record<string, EnrollmentBumpData>) {
-  const data: { x: string, y: number | null }[] = map(instructorData, (yearSemesterData, yearSemester) => {
-
-    const { total_dropped, total_headcount } = yearSemesterData;
-
-    return {
-      x: yearSemester,
-      y: round(total_dropped / total_headcount, 2) + (Math.random() / 100)
-    }
-  });
-
-
-    for (const offeredTime of offeredTimes) {
-      if ( !some(data, { x: offeredTime})) {
-        data.push( { x: offeredTime, y: null } )
-      }
-    }
-
-
-  return sortBy(data, (datum) => datum.x);
-}
-
-
-function groupEnrollmentByInstructor(coursedata: Courses | null): Record<string, Record<string, EnrollmentBumpData>> {
-  if (isNil(coursedata)) {
-    return {};
-  }
-
-  const data: CourseEnrollmentByInstructorInfo[] = coursedata?.stats.enrollment_by_instructor || [];
-  const groupedData: Dictionary<CourseEnrollmentByInstructorInfo[]> = groupBy(data, 'instructor');
-  const result: Record<string, Record<string, EnrollmentBumpData>> = {};
-
-  forEach(groupedData, (courses, instructor) => {
-
-    result[instructor] = {};
-
-    forEach(courses, (course) => {
-      const yearSemester = `${course.year}-${course.semester}`;
-
-      result[instructor][yearSemester] = course;
-    });
-  });
-
-  return result;
-}
 
 
 const theme = {
