d1de995611af09c50d9aff60483314e69307a021
Daria Cheatham <daria.cheatham@gmail.com>
2023-11-07 16:15:35 -0800

    Extract out CourseTable

---
+++ src/components/CourseTable.tsx
@@ -0,0 +1,54 @@
+import { map } from "lodash";
+import { Course, Courses } from "../data/models";
+import { dropRate } from "../util";
+
+export interface CourseTableProps {
+  data: Courses;
+}
+
+export function CourseTable(props: CourseTableProps) {
+  const { data } = props;
+
+  return <table className="table table-striped table-bordered">
+    <thead>
+      <tr>
+        <th>Year</th>
+        <th>Semester</th>
+        <th>Campus</th>
+        <th>Section</th>
+        <th>Course No</th>
+        <th>Instructor</th>
+        <th>Headcount</th>
+        <th>Dropped</th>
+        <th>Drop Rate</th>
+        <th>Meeting Times</th>
+      </tr>
+    </thead>
+    <tbody>
+      {map(data.courses, (course, index) => (
+        <CourseTableRow key={index} course={course} />
+      ))}
+    </tbody>
+  </table>;
+}
+
+export interface CourseTableRowProps {
+  course: Course;
+}
+
+export function CourseTableRow(props: CourseTableRowProps) {
+  const { course } = props;
+
+  return <tr>
+    <td>{course.year}</td>
+    <td>{course.semester}</td>
+    <td>{course.campus}</td>
+    <td>{course.section}</td>
+    <td>{course.course_no}</td>
+    <td>{course.instructor || 'N/A'}</td>
+    <td>{course.headcount}</td>
+    <td>{course.dropped}</td>
+    <td>{dropRate(course.headcount, course.dropped)}</td>
+    <td>{course.meeting_times || 'N/A'}</td>
+  </tr>;
+}
--- src/pages/CourseOverviewPage.tsx
+++ src/pages/CourseOverviewPage.tsx
@@ -1,10 +1,10 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { Course, Courses } from "../data/models";
+import { Courses } from "../data/models";
 import { fetchCourse } from "../data/dataFetch";
-import { first, map } from "lodash";
-import { dropRate } from "../util";
+import { first } from "lodash";
 import Breadcrumbs from "../components/Breadcrumbs";
+import { CourseTable } from "../components/CourseTable";
 
 export default function CourseOverviewPage()
 {
@@ -29,55 +29,4 @@ export default function CourseOverviewPage()
         <h1>{subject} {catalogNo} — {title}</h1>
         { courseData && <CourseTable data={courseData} />}
     </div>
-}
-
-interface CourseTableRowProps {
-    course: Course;
-}
-
-export function CourseTableRow(props: CourseTableRowProps) {
-    const { course } = props;
-
-    return <tr>
-      <td>{course.year}</td>
-      <td>{course.semester}</td>
-      <td>{course.campus}</td>
-      <td>{course.section}</td>
-      <td>{course.course_no}</td>
-      <td>{course.instructor || 'N/A'}</td>
-      <td>{course.headcount}</td>
-      <td>{course.dropped}</td>
-      <td>{dropRate(course.headcount, course.dropped)}</td>
-      <td>{course.meeting_times || 'N/A'}</td>
-    </tr>
-}
-  
-interface CourseTableProps {
-    data: Courses;
-}
-  
-export function CourseTable( props: CourseTableProps ) {
-    const { data } = props;
-
-    return <table className="table table-striped table-bordered">
-      <thead>
-        <tr>
-          <th>Year</th>
-          <th>Semester</th>
-          <th>Campus</th>
-          <th>Section</th>
-          <th>Course No</th>
-          <th>Instructor</th>
-          <th>Headcount</th>
-          <th>Dropped</th>
-          <th>Drop Rate</th>
-          <th>Meeting Times</th>
-        </tr>
-      </thead>
-      <tbody>
-        {map(data.courses, (course, index) => (
-          <CourseTableRow key={index} course={course} />
-        ))}
-      </tbody>
-    </table>
 }
