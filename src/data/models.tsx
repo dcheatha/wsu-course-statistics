0ca8a01a72103e75af20a97c028c1bb4d25d7fba
Daria Cheatham <daria.cheatham@gmail.com>
2023-11-07 18:50:52 -0800

    Rough in a bump chart

--- src/data/models.tsx
+++ src/data/models.tsx
@@ -36,6 +36,7 @@ export interface SubjectCourse {
 
 export interface Courses {
   courses: Course[];
+  stats: CourseStatistics;
 }
 
 export interface Course {
@@ -52,4 +53,27 @@ export interface Course {
   headcount: number;
   dropped: number;
   meeting_times?: string | null;
+}
+
+export interface CourseStatistics {
+  grades_by_instructor: CourseGradeByInstructorInfo[];
+  enrollment_by_instructor: CourseEnrollmentByInstructorInfo[];
+}
+
+export interface CourseGradeByInstructorInfo {
+  course_ids: number[];
+  year: number;
+  semester: string;
+  instructor: string | null;
+  headcount: number;
+  grade: number;
+}
+
+export interface CourseEnrollmentByInstructorInfo {
+  course_ids: number[];
+  year: number;
+  semester: string;
+  instructor: string | null;
+  total_headcount: number;
+  total_dropped: number;
 }
--- src/pages/CourseOverviewPage.tsx
+++ src/pages/CourseOverviewPage.tsx
@@ -1,10 +1,11 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { Courses } from "../data/models";
+import { Course, CourseEnrollmentByInstructorInfo, Courses } from "../data/models";
 import { fetchCourse } from "../data/dataFetch";
-import { first } from "lodash";
+import { Dictionary, first, forEach, groupBy, isNil, map, reverse, round, size, sortBy, union } from "lodash";
 import Breadcrumbs from "../components/Breadcrumbs";
 import { CourseTable } from "../components/CourseTable";
+import { ResponsiveBump } from "@nivo/bump";
 
 export default function CourseOverviewPage()
 {
@@ -27,6 +28,214 @@ export default function CourseOverviewPage()
     return <div>
         <Breadcrumbs subject={subject} catalogNo={catalogNo}/>
         <h1>{subject} {catalogNo} — {title}</h1>
+
+        <h2>Stats</h2>
+        <h3>Drop Rate</h3>
+        <DropRateBump data={courseData}/>
+
+        <h2>All Known Historical Offerings</h2>
         { courseData && <CourseTable data={courseData} />}
     </div>
+}
+
+export function DropRateBump( props: { data: Courses | null })
+{
+   const data = computeDropRateBumpData(props.data);
+   return <div style={{width: "100%", height: 50 * size(data)}}>
+    <ResponsiveBump
+        theme={theme}
+        data={data}
+        colors={{ scheme: 'spectral' }}
+        lineWidth={3}
+        activeLineWidth={6}
+      axisBottom={{
+          tickSize: 5,
+          tickPadding: 5,
+          tickRotation: 0,
+          legend: '',
+          legendPosition: 'middle',
+          legendOffset: 32
+      }}
+      axisLeft={{
+          tickSize: 5,
+          tickPadding: 5,
+          tickRotation: 0,
+          legend: 'Percent Dropped',
+          legendPosition: 'middle',
+          legendOffset: -40,
+          format: '.00%',
+      }}
+      margin={{ top: 40, right: 100, bottom: 40, left: 60 }}
+    />
+    </div>
+}
+
+
+interface EnrollmentBumpData {
+  total_headcount: number;
+  total_dropped: number;
+  course_ids: number[];
+}
+
+function computeDropRateBumpData(coursedata: Courses | null) {
+  const groupedByInstructor = groupEnrollmentByInstructor(coursedata);
+
+  const data = map(groupedByInstructor, (instructorData, instructor) => {
+    return {
+      id: instructor,
+      data: computeDropRateBumpDataForInstructor(instructorData)
+    }
+  });
+
+  console.log(data)
+  return data;
+}
+
+
+function computeDropRateBumpDataForInstructor(instructorData: Record<string, EnrollmentBumpData>) {
+  let data = map(instructorData, (yearSemesterData, yearSemester) => {
+
+    const { total_dropped, total_headcount } = yearSemesterData;
+
+    return {
+      x: yearSemester,
+      y: round(total_dropped / total_headcount, 2) + (Math.random() / 100)
+    }
+  });
+
+  return sortBy(data, (datum) => datum.x)
+}
+
+
+function groupEnrollmentByInstructor(coursedata: Courses | null): Record<string, Record<string, EnrollmentBumpData>> {
+  if (isNil(coursedata)) {
+    return {};
+  }
+
+  const data: CourseEnrollmentByInstructorInfo[] = coursedata?.stats.enrollment_by_instructor || [];
+  const groupedData: Dictionary<CourseEnrollmentByInstructorInfo[]> = groupBy(data, 'instructor');
+  const result: Record<string, Record<string, EnrollmentBumpData>> = {};
+
+  forEach(groupedData, (courses, instructor) => {
+
+    result[instructor] = {};
+
+    forEach(courses, (course) => {
+      const yearSemester = `${course.year}-${course.semester}`;
+
+      result[instructor][yearSemester] = course;
+    });
+  });
+
+  return result;
+}
+
+
+const theme = {
+  "background": "#ffffff",
+  "text": {
+      "fontSize": 11,
+      "fill": "#333333",
+      "outlineWidth": 0,
+      "outlineColor": "transparent"
+  },
+  "axis": {
+      "domain": {
+          "line": {
+              "stroke": "#777777",
+              "strokeWidth": 1
+          }
+      },
+      "legend": {
+          "text": {
+              "fontSize": 12,
+              "fill": "#333333",
+              "outlineWidth": 0,
+              "outlineColor": "transparent"
+          }
+      },
+      "ticks": {
+          "line": {
+              "stroke": "#777777",
+              "strokeWidth": 1
+          },
+          "text": {
+              "fontSize": 11,
+              "fill": "#333333",
+              "outlineWidth": 0,
+              "outlineColor": "transparent"
+          }
+      }
+  },
+  "grid": {
+      "line": {
+          "stroke": "#dddddd",
+          "strokeWidth": 1
+      }
+  },
+  "legends": {
+      "title": {
+          "text": {
+              "fontSize": 11,
+              "fill": "#333333",
+              "outlineWidth": 0,
+              "outlineColor": "transparent"
+          }
+      },
+      "text": {
+          "fontSize": 11,
+          "fill": "#333333",
+          "outlineWidth": 0,
+          "outlineColor": "transparent"
+      },
+      "ticks": {
+          "line": {},
+          "text": {
+              "fontSize": 10,
+              "fill": "#333333",
+              "outlineWidth": 0,
+              "outlineColor": "transparent"
+          }
+      }
+  },
+  "annotations": {
+      "text": {
+          "fontSize": 13,
+          "fill": "#333333",
+          "outlineWidth": 2,
+          "outlineColor": "#ffffff",
+          "outlineOpacity": 1
+      },
+      "link": {
+          "stroke": "#000000",
+          "strokeWidth": 1,
+          "outlineWidth": 2,
+          "outlineColor": "#ffffff",
+          "outlineOpacity": 1
+      },
+      "outline": {
+          "stroke": "#000000",
+          "strokeWidth": 2,
+          "outlineWidth": 2,
+          "outlineColor": "#ffffff",
+          "outlineOpacity": 1
+      },
+      "symbol": {
+          "fill": "#000000",
+          "outlineWidth": 2,
+          "outlineColor": "#ffffff",
+          "outlineOpacity": 1
+      }
+  },
+  "tooltip": {
+      "container": {
+          "background": "#ffffff",
+          "fontSize": 12
+      },
+      "basic": {},
+      "chip": {},
+      "table": {},
+      "tableCell": {},
+      "tableCellValue": {}
+  }
 }
